56bad68d4b2faf9e8314f43ff47586eede63afc8
Niklas Ekström <mail@niklasekstrom.nu>
2019-08-17 15:55:12 +0200

    Drive DRAM data pins for sufficiently long.
    More correctly drives the data pins during a read until casl/casu are negated, instead of until ras is negated. There is a small difference, and although the old logic has worked correctly, there could conceivably be individual machine for which the old logic leads to problems.

--- HDL/dram_port.v
+++ HDL/dram_port.v
@@ -1,7 +1,6 @@
 module dram_port(
     input               clk200,
 
-    //output              DR_XMEM,
     input               DR_WE_n,
     input               DR_RAS0_n,
     input               DR_RAS1_n,
@@ -22,8 +21,6 @@ module dram_port(
     input       [15:0]  dram_in_sram_out
     );
 
-    //assign DR_XMEM = 1'b0;
-
     wire ras0 = !DR_RAS0_n;
     wire ras1 = !DR_RAS1_n;
     wire casl = !DR_CASL_n;
@@ -33,18 +30,18 @@ module dram_port(
     wire rascas = ras && cas;
 
     reg [2:0] ras_sync = 3'd0;
+    reg [2:0] cas_sync = 3'd0;
     reg [2:0] rascas_sync = 3'd0;
 
     always @(posedge clk200)
     begin
         ras_sync <= {ras_sync[1:0], ras};
+        cas_sync <= {cas_sync[1:0], cas};
         rascas_sync <= {rascas_sync[1:0], rascas};
     end
 
-    wire ras_strobe = ras_sync[1] && !ras_sync[2];
-    wire rascas_strobe = rascas_sync[1] && !rascas_sync[2];
-
     reg dram_read = 1'b0;
+    reg drive_data = 1'b0;
     reg [18:0] dram_address = 19'd0;
     reg dram_lb = 1'b0;
     reg dram_ub = 1'b0;
@@ -60,7 +57,7 @@ module dram_port(
 
     always @(posedge clk200)
     begin
-        if (ras_strobe)
+        if (ras_sync[1] && !ras_sync[2])
         begin
             dram_address[15:8] <= DR_A[7:0];
             dram_address[16] <= DR_A[8];
@@ -68,7 +65,7 @@ module dram_port(
             dram_read <= DR_WE_n;
         end
 
-        if (rascas_strobe)
+        if (rascas_sync[1] && !rascas_sync[2])
         begin
             dram_address[7:0] <= DR_A[7:0];
             dram_address[17] <= DR_A[8];
@@ -76,11 +73,15 @@ module dram_port(
             dram_lb <= casl;
             dram_ub <= casu;
             dram_out_sram_in <= DR_D;
+            drive_data <= dram_read;
         end
+
+        if (!cas_sync[1] && cas_sync[2])
+            drive_data <= 1'b0;
     end
 
-    wire drivel = ras && casl && dram_read;
-    wire driveu = ras && casu && dram_read;
+    wire drivel = drive_data && casl;
+    wire driveu = drive_data && casu;
 
     assign DR_D[ 7:0] = drivel ? dram_in_sram_out[ 7:0] : 8'bz;
     assign DR_D[15:8] = driveu ? dram_in_sram_out[15:8] : 8'bz;
