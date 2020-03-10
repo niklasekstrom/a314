aed62bb798569ce28a87ada3b8887bfc85ee0898
Niklas Ekström <mail@niklasekstrom.nu>
2020-03-10 21:18:39 +0100

    Refactored bank mapping detection algorithm

--- Software/a314device/fix_mem_region.c
+++ Software/a314device/fix_mem_region.c
@@ -86,74 +86,118 @@ BOOL overlap(struct MemHeader *mh, ULONG lower, ULONG upper)
 	return lower < (ULONG)(mh->mh_Upper) && (ULONG)(mh->mh_Lower) < upper;
 }
 
+void mark_region_a314(ULONG address, ULONG size)
+{
+	struct List *memlist = &(SysBase->MemList);
+
+	for (struct Node *node = memlist->lh_Head; node->ln_Succ != NULL; node = node->ln_Succ)
+	{
+		struct MemHeader *mh = (struct MemHeader *)node;
+		if (overlap(mh, address, address + size))
+		{
+			if ((ULONG)mh->mh_Lower < address)
+			{
+				mh->mh_Attributes &= ~MEMF_A314;
+				mh = split_region(mh, address);
+			}
+			else
+				Remove((struct Node *)mh);
+
+			if (address + size < (ULONG)mh->mh_Upper)
+			{
+				struct MemHeader *new_mh = split_region(mh, address + size);
+				new_mh->mh_Attributes &= ~MEMF_A314;
+				Enqueue(memlist, (struct Node *)new_mh);
+			}
+
+			mh->mh_Node.ln_Pri = -20;
+			mh->mh_Attributes |= MEMF_A314;
+			Enqueue(memlist, (struct Node *)mh);
+			return;
+		}
+	}
+}
+
+void detect_block_mapping_heuristically(ULONG present_blocks)
+{
+	if ((present_blocks & 0xc) == 0xc)
+	{
+		lower_bank_address = ONE_MB;
+		upper_bank_address = ONE_MB + HALF_MB;
+	}
+	else if (present_blocks & 0x01000000)
+		lower_bank_address = 0xc00000;
+	else if (present_blocks & 0x00000002)
+		lower_bank_address = HALF_MB;
+}
+
+ULONG get_present_blocks()
+{
+	ULONG present_blocks = 0;
+
+	for (struct Node *node = SysBase->MemList.lh_Head; node->ln_Succ != NULL; node = node->ln_Succ)
+	{
+		struct MemHeader *mh = (struct MemHeader *)node;
+		ULONG block = (ULONG)mh->mh_Lower >> 19;
+		ULONG address = block << 19;
+		while (address < (ULONG)mh->mh_Upper)
+		{
+			present_blocks |= 1UL << block;
+			block++;
+			address = block << 19;
+		}
+	}
+
+	return present_blocks;
+}
+
 BOOL fix_memory()
 {
 	Forbid();
 
-	struct List *memlist = &(SysBase->MemList);
-	struct Node *node;
-	struct MemHeader *mh;
+	ULONG present_blocks = get_present_blocks();
 
-	for (node = memlist->lh_Head; node->ln_Succ != NULL; node = node->ln_Succ)
+	// Only test blocks in chip mem and slow mem ranges.
+	present_blocks &= 0x0700000f; // 0b00000111000000000000000000001111
+
+	detect_block_mapping_heuristically(present_blocks);
+
+	if (lower_bank_address == -1 && upper_bank_address == -1)
 	{
-		mh = (struct MemHeader *)node;
-		if (mh->mh_Attributes & MEMF_A314)
-		{
-			lower_bank_address = (ULONG)(mh->mh_Lower) & ~(HALF_MB - 1);
-			if (lower_bank_address == ONE_MB)
-				upper_bank_address = lower_bank_address + HALF_MB;
-
-			Permit();
-			return TRUE;
-		}
-	}
-
-	for (node = memlist->lh_Head; node->ln_Succ != NULL; node = node->ln_Succ)
-	{
-		mh = (struct MemHeader *)node;
-		if (overlap(mh, 0xc00000, 0xc80000))
-		{
-			Remove((struct Node *)mh);
-			mh->mh_Node.ln_Pri = -20;
-			mh->mh_Attributes |= MEMF_A314;
-			Enqueue(memlist, (struct Node*)mh);
-
-			lower_bank_address = 0xc00000;
-
-			Permit();
-			return TRUE;
-		}
-	}
-
-	struct MemHeader *chip_mh = NULL;
-
-	for (node = memlist->lh_Head; node->ln_Succ != NULL; node = node->ln_Succ)
-	{
-		mh = (struct MemHeader *)node;
-		if (overlap(mh, 0x0, 0x200000))
-		{
-			chip_mh = mh;
-			break;
-		}
-	}
-
-	if (chip_mh == NULL || (ULONG)(chip_mh->mh_Upper) <= 0x80000)
-	{
+		// Didn't detect any A314 memory; the a314.device Open() should fail.
+		// Should log the reason why this happened.
 		Permit();
 		return FALSE;
 	}
 
-	// Split chip memory region into motherboard and A314 memory regions.
-	ULONG split_at = (ULONG)(chip_mh->mh_Upper) > ONE_MB ? ONE_MB : HALF_MB;
+	if (lower_bank_address == -1)
+	{
+		mark_region_a314(upper_bank_address, HALF_MB);
+	}
+	else if (upper_bank_address == -1)
+	{
+		mark_region_a314(lower_bank_address, HALF_MB);
+	}
+	else
+	{
+		ULONG lo = lower_bank_address;
+		ULONG hi = upper_bank_address;
+		if (lo > hi)
+		{
+			lo = upper_bank_address;
+			hi = lower_bank_address;
+		}
 
-	mh = split_region(chip_mh, split_at);
-	mh->mh_Node.ln_Pri = -20;
-	mh->mh_Attributes |= MEMF_A314;
-	Enqueue(memlist, (struct Node*)mh);
-
-	lower_bank_address = split_at;
-	if (split_at == ONE_MB)
-		upper_bank_address = lower_bank_address + HALF_MB;
+		if (lo + HALF_MB == hi)
+		{
+			mark_region_a314(lo, ONE_MB);
+		}
+		else
+		{
+			mark_region_a314(lo, HALF_MB);
+			mark_region_a314(hi, HALF_MB);
+		}
+	}
 
 	Permit();
 	return TRUE;
