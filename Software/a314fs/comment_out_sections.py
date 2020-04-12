efb3549b3114134b3f4593f9431ed4d2b0073e2a
Niklas Ekström <mail@niklasekstrom.nu>
2020-04-12 22:29:58 +0200

    Fix a314fs build script for Python 3 in Windows

--- Software/a314fs/comment_out_sections.py
+++ Software/a314fs/comment_out_sections.py
@@ -1,5 +1,5 @@
 with open('a314fs.asm', 'r+b') as f:
-    text = f.read()
+    text = f.read().decode('utf-8')
     text = text.replace('section', ';section')
     f.seek(0, 0)
-    f.write(text)
+    f.write(text.encode('utf-8'))
