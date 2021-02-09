e7275f2711fe32fc027fddd7b827e77fee04cfa5
Niklas Ekström <mail@niklasekstrom.nu>
2021-02-09 08:43:47 +0100

    Add support for SPI protocol versions to diag

--- Software/diag/read_back_test.py
+++ Software/diag/read_back_test.py
@@ -12,11 +12,26 @@ WRITE_SRAM_CMD = 1
 READ_CMEM_CMD = 2
 WRITE_CMEM_CMD = 3
 
-def read(addr, n):
-    return spi.xfer([(READ_SRAM_CMD << 4) | (addr >> 16) & 0xf, (addr >> 8) & 0xff, addr & 0xff, 0] + [0] * n)[4:]
+def read_protocol_version():
+    return spi.xfer([255, 0])[1]
 
-def write(addr, data):
-    spi.xfer([(WRITE_SRAM_CMD << 4) | ((addr >> 16) & 0xf), (addr >> 8) & 0xff, addr & 0xff] + data)
+proto_ver = read_protocol_version()
+if proto_ver not in [0, 1]:
+    raise RuntimeError("Unknown protocol version: {}".format(proto_ver))
+
+print("Uses protocol version: {}".format(proto_ver))
+
+def read_sram(addr, n):
+    if proto_ver == 0:
+        return spi.xfer([(READ_SRAM_CMD << 4) | (addr >> 16) & 0xf, (addr >> 8) & 0xff, addr & 0xff, 0] + [0] * n)[4:]
+    elif proto_ver == 1:
+        return spi.xfer([(READ_SRAM_CMD << 5) | (addr >> 16) & 0x1f, (addr >> 8) & 0xff, addr & 0xff, 0] + [0] * n)[4:]
+
+def write_sram(addr, data):
+    if proto_ver == 0:
+        spi.xfer([(WRITE_SRAM_CMD << 4) | ((addr >> 16) & 0xf), (addr >> 8) & 0xff, addr & 0xff] + data)
+    elif proto_ver == 1:
+        spi.xfer([(WRITE_SRAM_CMD << 5) | ((addr >> 16) & 0x1f), (addr >> 8) & 0xff, addr & 0xff] + data)
 
 def g(n):
     w = 0
@@ -31,6 +46,6 @@ data = list(g(65536))
 data = data * 4
 
 for i in range(32):
-    write(i*2048, data[i*2048:(i+1)*2048])
+    write_sram(i*2048, data[i*2048:(i+1)*2048])
 
-print(read(0, 2048))
+print(read_sram(0, 2048))
