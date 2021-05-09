f41ca771e5785df951196284e00fc5154125b05d
Niklas Ekström <mail@niklasekstrom.nu>
2021-05-09 12:24:42 +0200

    Removed global variables in device.c

--- Software/a314device/device.c
+++ Software/a314device/device.c
@@ -16,34 +16,32 @@
 char device_name[] = A314_NAME;
 char id_string[] = A314_NAME " 1.0 (25 Aug 2018)";
 
-BPTR saved_seg_list;
-BOOL running = FALSE;
-
-static struct Library *init_device(__reg("a6") struct ExecBase *sys_base, __reg("a0") BPTR seg_list, __reg("d0") struct Library *dev)
+static struct Library *init_device(__reg("a6") struct ExecBase *sys_base, __reg("a0") BPTR seg_list, __reg("d0") struct A314Device *dev)
 {
-	saved_seg_list = seg_list;
+	dev->saved_seg_list = seg_list;
+	dev->running = FALSE;
 
 	// We are being called from InitResident() in initializers.asm.
 	// MakeLibrary() was executed before we got here.
 
-	dev->lib_Node.ln_Type = NT_DEVICE;
-	dev->lib_Node.ln_Name = device_name;
-	dev->lib_Flags = LIBF_SUMUSED | LIBF_CHANGED;
-	dev->lib_Version = 1;
-	dev->lib_Revision = 0;
-	dev->lib_IdString = (APTR)id_string;
+	dev->lib.lib_Node.ln_Type = NT_DEVICE;
+	dev->lib.lib_Node.ln_Name = device_name;
+	dev->lib.lib_Flags = LIBF_SUMUSED | LIBF_CHANGED;
+	dev->lib.lib_Version = 1;
+	dev->lib.lib_Revision = 0;
+	dev->lib.lib_IdString = (APTR)id_string;
 
 	// AddDevice() is executed after we return.
-	return dev;
+	return &dev->lib;
 }
 
-static BPTR expunge(__reg("a6") struct Library *dev)
+static BPTR expunge(__reg("a6") struct A314Device *dev)
 {
 	// There is currently no support for unloading a314.device.
 
 	if (TRUE) //dev->lib_OpenCnt != 0)
 	{
-		dev->lib_Flags |= LIBF_DELEXP;
+		dev->lib.lib_Flags |= LIBF_DELEXP;
 		return 0;
 	}
 
@@ -55,46 +53,46 @@ static BPTR expunge(__reg("a6") struct Library *dev)
 	*/
 }
 
-static void open(__reg("a6") struct Library *dev, __reg("a1") struct A314_IORequest *ior, __reg("d0") ULONG unitnum, __reg("d1") ULONG flags)
+static void open(__reg("a6") struct A314Device *dev, __reg("a1") struct A314_IORequest *ior, __reg("d0") ULONG unitnum, __reg("d1") ULONG flags)
 {
-	dev->lib_OpenCnt++;
+	dev->lib.lib_OpenCnt++;
 
-	if (dev->lib_OpenCnt == 1 && !running)
+	if (dev->lib.lib_OpenCnt == 1 && !dev->running)
 	{
 		if (!task_start())
 		{
 			ior->a314_Request.io_Error = IOERR_OPENFAIL;
 			ior->a314_Request.io_Message.mn_Node.ln_Type = NT_REPLYMSG;
-			dev->lib_OpenCnt--;
+			dev->lib.lib_OpenCnt--;
 			return;
 		}
-		running = TRUE;
+		dev->running = TRUE;
 	}
 
 	ior->a314_Request.io_Error = 0;
 	ior->a314_Request.io_Message.mn_Node.ln_Type = NT_REPLYMSG;
 }
 
-static BPTR close(__reg("a6") struct Library *dev, __reg("a1") struct A314_IORequest *ior)
+static BPTR close(__reg("a6") struct A314Device *dev, __reg("a1") struct A314_IORequest *ior)
 {
 	ior->a314_Request.io_Device = NULL;
 	ior->a314_Request.io_Unit = NULL;
 
-	dev->lib_OpenCnt--;
+	dev->lib.lib_OpenCnt--;
 
-	if (dev->lib_OpenCnt == 0 && (dev->lib_Flags & LIBF_DELEXP))
+	if (dev->lib.lib_OpenCnt == 0 && (dev->lib.lib_Flags & LIBF_DELEXP))
 		return expunge(dev);
 
 	return 0;
 }
 
-static void begin_io(__reg("a6") struct Library *dev, __reg("a1") struct A314_IORequest *ior)
+static void begin_io(__reg("a6") struct A314Device *dev, __reg("a1") struct A314_IORequest *ior)
 {
 	PutMsg(&task_mp, (struct Message *)ior);
 	ior->a314_Request.io_Flags &= ~IOF_QUICK;
 }
 
-static ULONG abort_io(__reg("a6") struct Library *dev, __reg("a1") struct A314_IORequest *ior)
+static ULONG abort_io(__reg("a6") struct A314Device *dev, __reg("a1") struct A314_IORequest *ior)
 {
 	// There is currently no support for aborting an IORequest.
 	return IOERR_NOCMD;
@@ -114,7 +112,7 @@ static ULONG device_vectors[] =
 
 ULONG auto_init_tables[] =
 {
-	sizeof(struct Library),
+	sizeof(struct A314Device),
 	(ULONG)device_vectors,
 	0,
 	(ULONG)init_device,
--- Software/a314device/device.h
+++ Software/a314device/device.h
@@ -1,2 +1,19 @@
+#ifndef __A314_DEVICE_H
+#define __A314_DEVICE_H
+
+#include <exec/types.h>
+#include <exec/libraries.h>
+#include <libraries/dos.h>
+
+struct A314Device
+{
+    struct Library lib;
+
+    BPTR saved_seg_list;
+    BOOL running;
+};
+
 extern char device_name[];
 extern char id_string[];
+
+#endif /* __A314_DEVICE_H */
